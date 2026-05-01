47ab48e24207411bd3e8e4b3750486b56acbc0f7
Sathya M <sathyasweety3573@gmail.com>
2026-05-01 11:20:04 +0530

    fixed cors issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,66 +6,75 @@ import {
 } from "react-router-dom";
 
 import SignUp from "./Pages/SignUp";
+
 import SignIn from "./Pages/SignIn";
+
 import ForgotPassword from "./Pages/ForgotPassword";
+
 import CheckEmail from "./Pages/CheckEmail";
+
 import ResetPassword from "./Pages/ResetPassword";
+
 import WelcomeBlog from "./Pages/Welcome";
+
 import SuccessReset from "./Pages/SuccessReset";
 
 function App() {
 
   return (
 
-    <div>
+    <Routes>
 
-      <Routes>
+      {/* SIGN IN */}
 
-        {/* SIGN IN */}
-        <Route
-          path="/"
-          element={<SignIn />}
-        />
+      <Route
+        path="/"
+        element={<SignIn />}
+      />
 
-        {/* SIGN UP */}
-        <Route
-          path="/signup"
-          element={<SignUp />}
-        />
+      {/* SIGN UP */}
 
-        {/* FORGOT PASSWORD */}
-        <Route
-          path="/forgot-password"
-          element={<ForgotPassword />}
-        />
+      <Route
+        path="/signup"
+        element={<SignUp />}
+      />
 
-        {/* CHECK EMAIL */}
-        <Route
-          path="/check-email"
-          element={<CheckEmail />}
-        />
+      {/* FORGOT PASSWORD */}
 
-        {/* RESET PASSWORD */}
-        <Route
-          path="/reset-password/:token"
-          element={<ResetPassword />}
-        />
+      <Route
+        path="/forgot-password"
+        element={<ForgotPassword />}
+      />
 
-        {/* SUCCESS PAGE */}
-        <Route
-          path="/success"
-          element={<SuccessReset />}
-        />
+      {/* CHECK EMAIL */}
 
-        {/* WELCOME PAGE */}
-        <Route
-          path="/welcome"
-          element={<WelcomeBlog />}
-        />
+      <Route
+        path="/check-email"
+        element={<CheckEmail />}
+      />
 
-      </Routes>
+      {/* RESET PASSWORD */}
 
-    </div>
+      <Route
+        path="/reset-password/:token"
+        element={<ResetPassword />}
+      />
+
+      {/* SUCCESS PAGE */}
+
+      <Route
+        path="/success"
+        element={<SuccessReset />}
+      />
+
+      {/* WELCOME PAGE */}
+
+      <Route
+        path="/welcome"
+        element={<WelcomeBlog />}
+      />
+
+    </Routes>
 
   );
 
